cd0c6fcd441eadd392864c0fc66517efd60c0f53
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2024-07-16 22:25:56 +0530

    feat(/planets): implemented server for get planet request

---
+++ server/src/routes/planets/planets.router.js
@@ -0,0 +1,11 @@
+const express=require('express');
+
+const {
+    getAllPlanets,
+}=require('./planets.controller');
+
+const plantesRouter=express.Router();
+
+plantesRouter.get('/planets',getAllPlanets);
+
+module.exports=plantesRouter;
